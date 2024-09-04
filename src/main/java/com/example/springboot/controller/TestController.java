bf19d49933acec6a2284c12dda4db2d962730184
b1na2 <463752413@qq.com>
2024-09-04 19:49:56 +0800

    Update TestController.java

--- src/main/java/com/example/springboot/controller/TestController.java
+++ src/main/java/com/example/springboot/controller/TestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class TestController {
 
-    private static final String INTERNAL_IP = "10.137.166.157:3000";
+    private static final String INTERNAL_IP = "10.137.175.218:3000";
     private static final String DOMAIN_NAME = "network-53-0:3000";
 
     @GetMapping("/")
